ba5ca91cdc10a7127c81996660445c957b16aa44
chocsx <chocsx@hotmail.com>
2014-11-20 00:33:35 -0200

    Mapa Aparecendo
    consegui colocar o mapa no app :dancer:

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -12,7 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.Devices.Geolocation;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
@@ -21,6 +23,7 @@ namespace App1
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// public sealed class MapControl : Control
     public sealed partial class MainPage : Page
     {
         Geolocator geo = null;
@@ -29,8 +32,7 @@ namespace App1
             this.InitializeComponent();
         }
 
-        private async void button1_Click(
-            object sender, RoutedEventArgs e)
+        private async void button1_Click(object sender, RoutedEventArgs e)
         {
             if (geo == null)
             {
@@ -42,6 +44,31 @@ namespace App1
             textLongitude.Text = "Longitude: " + pos.Coordinate.Point.Position.Longitude.ToString();
             textAccuracy.Text = "Accuracy: " + pos.Coordinate.Accuracy.ToString();
         }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            MapControl1.Center =
+                new Geopoint(new BasicGeoposition()
+                {
+                    Latitude = 47.604,
+                    Longitude = -122.329
+                });
+            MapControl1.ZoomLevel = 12;
+            MapControl1.LandmarksVisible = true;
+        }
+ 
+/*        private void AddMapIcon()
+        {
+            MapIcon MapIcon1 = new MapIcon();
+            MapIcon1.Location = new Geopoint(new BasicGeoposition()
+            {
+                Latitude = 47.620,
+                Longitude = -122.349
+            });
+            MapIcon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
+            MapIcon1.Title = "Space Needle";
+            MapControl1.MapElements.Add(MapIcon1);
+        }
+        */
 
     }
 }
